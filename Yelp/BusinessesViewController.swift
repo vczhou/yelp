c485fb072ea2620df6b28ec90bb0618995e70f88
vczhou <victoria.ch.zhou@gmail.com>
2017-02-16 22:29:49 -0600

    added navigation bar search controller

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -13,6 +13,9 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
     @IBOutlet weak var tableView: UITableView!
     var businesses: [Business]!
     
+    var searchBar: UISearchBar!
+    var searchString: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -37,6 +40,14 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
             }
         )
         
+        // Initialize the UISearchBar
+        searchBar = UISearchBar()
+        searchBar.delegate = self
+        
+        // Add SearchBar to the NavigationBar
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+        
         /* Example of Yelp search with more search options specified
          Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
          self.businesses = businesses
@@ -48,6 +59,30 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
          }
          */
         
+    }
+    
+    // Perform the search.
+    func doSearch() {
+        print(searchString)
+        
+        //MBProgressHUD.showAdded(to: self.view, animated: true)
+        
+        // Perform request to Yelp API
+        Business.searchWithTerm(term: searchString, completion: { (businesses: [Business]?, error: Error?) -> Void in
+            
+            self.businesses = businesses
+            self.tableView.reloadData()
+            
+            if let businesses = businesses {
+                for business in businesses {
+                    print(business.name!)
+                    print(business.address!)
+                }
+            }
+            
+        }
+        )
+
     }
     
     override func didReceiveMemoryWarning() {
@@ -81,4 +116,42 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
      }
      */
     
+}
+
+// SearchBar methods
+extension BusinessesViewController: UISearchBarDelegate {
+    
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true
+    }
+    
+    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(false, animated: true)
+        return true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if !(searchBar.text?.isEmpty)! {
+            searchString = searchBar.text!
+            print(searchString + "\n\n")
+        }
+        searchBar.resignFirstResponder()
+        doSearch()
+    }
+    
+    /*
+    // Add if want to search as typing
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if !searchText.isEmpty {
+            searchString = searchText
+            doSearch()
+        }
+    } */
 }
