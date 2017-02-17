05cc307e1f2deb71cf3d561d905f3b7c54f477e6
vczhou <victoria.ch.zhou@gmail.com>
2017-02-16 21:46:03 -0600

    address label wrap

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -35,6 +35,13 @@ class BusinessCell: UITableViewCell {
         // Initialization code
         thumbImageView.layer.cornerRadius = 3
         thumbImageView.clipsToBounds = true
+        
+        // nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
